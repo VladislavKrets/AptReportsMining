692ba32768fa8cdc314b9bc593683410434c1ad9
Vladislav Krets <vladislav.krets@gmail.com>
2021-04-10 18:56:43 +0300

    cleaned bot code

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import nltk
-import numpy as np
-import random
-import string # to process standard python strings
+import string
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# nltk.download() припервом запуске раскомментить и скачать всё
+# nltk.download() при первом запуске раскомментить и скачать всё
 
 f = open('chatbot.txt', 'r', errors='ignore')
 raw = f.read()
-raw = raw.lower() # converts to lowercase
-sent_tokens = nltk.sent_tokenize(raw, language='russian')# converts to list of sentences
-word_tokens = nltk.word_tokenize(raw, language='russian')# converts to list of words
+raw = raw.lower()
+sent_tokens = nltk.sent_tokenize(raw, language='russian')  # converts to list of sentences
+word_tokens = nltk.word_tokenize(raw, language='russian')  # converts to list of words
 lemmer = nltk.stem.WordNetLemmatizer()
-print('dwedwed')
 
-#WordNet is a semantically-oriented dictionary of English included in NLTK.
+
 def LemTokens(tokens):
     return [lemmer.lemmatize(token) for token in tokens]
 
@@ -26,56 +25,32 @@ def LemNormalize(text):
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
 
-GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
-GREETING_RESPONSES = ["hi", "hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
-
-
-def greeting(sentence):
-    for word in sentence.split():
-        if word.lower() in GREETING_INPUTS:
-            return random.choice(GREETING_RESPONSES)
-
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-
-def response(user_response):
-    robo_response=''
+def response():
+    robo_response = ''
     russian_stopwords = stopwords.words("russian")
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words=None)
     TfidfVec.stop_words_ = russian_stopwords
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
-    idx=vals.argsort()[0][-2]
+    idx = vals.argsort()[0][-2]
     flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]
 
     if req_tfidf == 0:
-        robo_response=robo_response+"I am sorry! I don't understand you"
+        robo_response = robo_response + "Я вас не понял"
         return robo_response
     else:
-        robo_response = robo_response+sent_tokens[idx]
+        robo_response = robo_response + sent_tokens[idx]
         return robo_response
 
-flag=True
-print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
-while flag==True:
+
+flag = True
+while flag:
     user_response = input()
-    user_response=user_response.lower()
-    if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
-            flag=False
-            print("ROBO: You are welcome..")
-        else:
-            if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
-            else:
-                sent_tokens.append(user_response)
-                word_tokens=word_tokens+nltk.word_tokenize(user_response)
-                final_words=list(set(word_tokens))
-                print("ROBO: ",end="")
-                print(response(user_response))
-                sent_tokens.remove(user_response)
-    else:
-        flag=False
-        print("ROBO: Bye! take care..")
+    user_response = user_response.lower()
+    sent_tokens.append(user_response)
+    word_tokens = word_tokens + nltk.word_tokenize(user_response)
+    final_words = list(set(word_tokens))
+    print(response())
+    sent_tokens.remove(user_response)
